6060fdd72b6e92ee9e25fcec7a381cad6ca9a4d4
magicalmeghan <magicalmeghan@google.com>
2020-07-17 12:30:27 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/recyclersample/FlowerAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.recyclersample
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class FlowerAdapter(val wordList: List<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val wordText:TextView = itemView.findViewById(R.id.flower)
+
+
+        fun bind(word: String){
+            wordText.text = word
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val inflator = LayoutInflater.from(parent.context)
+            .inflate(R.layout.flower_item, parent, false)
+
+        return CustomViewHolder(inflator)
+    }
+
+    override fun getItemCount(): Int {
+        return wordList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as CustomViewHolder).bind(wordList[position])
+    }
+}
